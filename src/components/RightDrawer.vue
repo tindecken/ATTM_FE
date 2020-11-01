bac8e99bd7498061b9451a803a932db6e2ff3199
Hoang.Nguyen <hoang.nguyen@hansencx.com>
2020-11-01 17:23:21 +0700

    so far so gôd

--- src/components/RightDrawer.vue
+++ src/components/RightDrawer.vue
@@ -30,6 +30,7 @@
           label="Feature"
           @filter="filterFeature"
           @filter-abort="abortFilterFeature"
+          @input="val => { onFeatureChange() }"
           use-input
           hide-selected
           fill-input
@@ -48,8 +49,8 @@
         <q-table
           dense
           title="Keywords"
-          :data="categories"
-          :columns="categoriesColumns"
+          :data="keywordDatas"
+          :columns="keywordColumns"
           row-key="name"
           :hide-pagination="true"
         />
@@ -74,7 +75,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 import {
-  defineComponent, reactive, ref,
+  defineComponent, reactive, Ref, ref,
 } from '@vue/composition-api';
 
 export default defineComponent({
@@ -95,15 +96,14 @@ export default defineComponent({
     const isShowOwner = ref(true);
     const isShowCreatedDate = ref(false);
     const allKeyword = ref();
-    const keywordColumns = ref();
-    const categoriesColumns = [
+    const keywordColumns = [
       {
         name: 'no',
         required: true,
         label: 'No',
         align: 'left',
         // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-        field: 'index',
+        field: 'rowIndex',
         sortable: true,
       },
       {
@@ -112,143 +112,26 @@ export default defineComponent({
         label: 'Name',
         align: 'left',
         // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-        field: (row: { name: any }) => row.name,
+        field: 'Name',
         format: (val: any) => `${val}`,
         sortable: true,
       },
       {
-        name: 'calories',
-        align: 'center',
+        name: 'description',
+        align: 'left',
         label: 'Description',
-        field: 'calories',
+        field: 'Description',
         sortable: true,
       },
       {
-        name: 'fat',
-        label: 'Owner',
-        field: 'fat',
+        name: 'updatedMessage',
+        align: 'left',
+        label: 'Updated Message',
+        field: 'UpdatedMessage',
         sortable: true,
-      },
-      { name: 'carbs', label: 'Created Date', field: 'carbs' },
-      { name: 'protein', label: 'Image', field: 'protein' },
-      { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
-      {
-        name: 'calcium',
-        label: 'Calcium (%)',
-        field: 'calcium',
-        sortable: true,
-        sort: (a: string, b: string) => parseInt(a, 10) - parseInt(b, 10),
-      },
-      {
-        name: 'iron',
-        label: 'Iron (%)',
-        field: 'iron',
-        sortable: true,
-        sort: (a: string, b: string) => parseInt(a, 10) - parseInt(b, 10),
       },
     ];
-    const keywordDatas = [
-      {
-        name: 'Frozen Yogurt',
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-        sodium: 87,
-        calcium: '14%',
-        iron: '1%',
-      },
-      {
-        name: 'Ice cream sandwich',
-        calories: 237,
-        fat: 9.0,
-        carbs: 37,
-        protein: 4.3,
-        sodium: 129,
-        calcium: '8%',
-        iron: '1%',
-      },
-      {
-        name: 'Eclair',
-        calories: 262,
-        fat: 16.0,
-        carbs: 23,
-        protein: 6.0,
-        sodium: 337,
-        calcium: '6%',
-        iron: '7%',
-      },
-      {
-        name: 'Cupcake',
-        calories: 305,
-        fat: 3.7,
-        carbs: 67,
-        protein: 4.3,
-        sodium: 413,
-        calcium: '3%',
-        iron: '8%',
-      },
-      {
-        name: 'Gingerbread',
-        calories: 356,
-        fat: 16.0,
-        carbs: 49,
-        protein: 3.9,
-        sodium: 327,
-        calcium: '7%',
-        iron: '16%',
-      },
-      {
-        name: 'Jelly bean',
-        calories: 375,
-        fat: 0.0,
-        carbs: 94,
-        protein: 0.0,
-        sodium: 50,
-        calcium: '0%',
-        iron: '0%',
-      },
-      {
-        name: 'Lollipop',
-        calories: 392,
-        fat: 0.2,
-        carbs: 98,
-        protein: 0,
-        sodium: 38,
-        calcium: '0%',
-        iron: '2%',
-      },
-      {
-        name: 'Honeycomb',
-        calories: 408,
-        fat: 3.2,
-        carbs: 87,
-        protein: 6.5,
-        sodium: 562,
-        calcium: '0%',
-        iron: '45%',
-      },
-      {
-        name: 'Donut',
-        calories: 452,
-        fat: 25.0,
-        carbs: 51,
-        protein: 4.9,
-        sodium: 326,
-        calcium: '2%',
-        iron: '22%',
-      },
-      {
-        name: 'KitKat',
-        calories: 518,
-        fat: 26.0,
-        carbs: 65,
-        protein: 7,
-        sodium: 54,
-        calcium: '12%',
-        iron: '6%',
-      },
-    ];
+    const keywordDatas: Ref<any[]> = ref([]);
     allKeyword.value = context.root.$store.getters['keyword/keywords'];
     categories.options = allKeyword.value.categories.map((c: { Name: string; }) => c.Name);
     allKeyword.value.categories.forEach((c: { Features: []; }) => {
@@ -329,6 +212,42 @@ export default defineComponent({
       console.log('delayed filter aborted')
     }
 
+    function onFeatureChange() {
+      console.log('selectedFeature', features.selectedFeature)
+      keywordDatas.value = [];
+      if (categories.selectedCategory == null) {
+        if (features.selectedFeature == null) {
+          // Todo: Load all keywords
+          allKeyword.value.categories.forEach((c: { Features: []; }) => {
+            c.Features.forEach((f: { Keywords: []; }) => {
+              f.Keywords.forEach((k: any) => {
+                keywordDatas.value.push(k);
+              })
+            });
+          });
+          keywordDatas.value = keywordDatas.value.map((v: any, i: number) => ({ ...v, rowIndex: i + 1 }))
+        } else {
+          // Todo: Load all keywords base on Feature
+          allKeyword.value.categories.forEach((c: { Features: []; }) => {
+            c.Features.forEach((f: any) => {
+              if (f.Name === features.selectedFeature) {
+                f.Keywords.forEach((k: any) => {
+                  keywordDatas.value.push(k)
+                })
+              }
+            });
+          });
+          console.log('keywordDatas', keywordDatas.value)
+          keywordDatas.value = keywordDatas.value.map((v: any, i: number) => ({ ...v, rowIndex: i + 1 }))
+          console.log('keywordDatas', keywordDatas.value)
+        }
+      } else if (features.selectedFeature == null) {
+        // Todo
+      } else {
+        // Todo
+      }
+    }
+
     return {
       kwFilter,
       selectedCategory,
@@ -337,7 +256,6 @@ export default defineComponent({
       features,
       isShowOwner,
       isShowCreatedDate,
-      categoriesColumns,
       keywordDatas,
       keywordColumns,
       filterCategory,
@@ -345,6 +263,7 @@ export default defineComponent({
       onCategoryChange,
       filterFeature,
       abortFilterFeature,
+      onFeatureChange,
     };
   },
 });
